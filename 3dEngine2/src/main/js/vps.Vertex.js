9087f117c6415ed92fab6d072aacd550ab8e9103
tomblackford <tom.blackford@vantagepointsolutions.co.uk>
2013-01-22 20:55:04 +0000

    Fix to FOV calculation. Updated control mechanism

--- 3dEngine2/src/main/js/vps.Vertex.js
+++ 3dEngine2/src/main/js/vps.Vertex.js
@@ -27,6 +27,7 @@ vps.Vertex = function(coords){
  * @param worldPosition
  */
 vps.Vertex.prototype.updateAbsoluteCoords = function(transformationMatrix, worldPosition){
+	
 	// Apply the local rotation first
 	transformationMatrix.apply(this.relativeCoords, this.absoluteCoords);
 	
@@ -59,13 +60,12 @@ vps.Vertex.prototype.updatePOVCoords = function(cameraRotationTransformationMatr
 	
 	// Work out if this vertex is within the camera's field of view 
 	// based on http://nic-gamedev.blogspot.co.uk/2011/11/using-vector-mathematics-and-bit-of.html
-	// TODO: IS THIS REALLY WORKING?
 	var inverseDistance = 1/this.distanceToCamera;
 	var vectorToPoint = new vps.Vector3d(distanceVector.x * inverseDistance, distanceVector.y * inverseDistance, distanceVector.z * inverseDistance);
 	var cameraVector = cameraRotation.toVector();
 	
 	var dotProduct = cameraVector.dotProduct(vectorToPoint);
-	var halfOfFOV = Math.PI/7;
+	var halfOfFOV = Math.PI/4;
 
 	if(dotProduct >= Math.cos(halfOfFOV)){
 		this.insideFieldOfView = true;
@@ -81,7 +81,7 @@ vps.Vertex.prototype.updatePOVCoords = function(cameraRotationTransformationMatr
 vps.Vertex.prototype.updateViewCoords = function(viewerPostion){
 	
 	var oneOverZ = 1/this.povCoords.z;
-	this.viewCoords.x = this.povCoords.x * 800 * oneOverZ + 250 ;
+	this.viewCoords.x = this.povCoords.x * 800 * oneOverZ + 500 ;
 	this.viewCoords.y = -this.povCoords.y * 800 * oneOverZ + 250 ;
 
 };
